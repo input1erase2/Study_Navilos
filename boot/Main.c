5d3d6140ca6cd76553b1d9d08326bafee62ffa72
soredemo <soredemo@naver.com>
2025-03-06 16:15:30 +0900

    3_Main_function_included

---
+++ boot/Main.c
@@ -0,0 +1,12 @@
+#include "stdint.h"
+#include "HalUart.h"
+
+static void hw_init(void);
+
+void main(void) {
+    hw_init();
+    uint32_t i = 100;
+    while (i--) {
+        hal_uart_put_char('C');
+    }
+}
